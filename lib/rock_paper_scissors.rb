38eac83451b9d89d5802d892099452029d493e0b
Ron Craig <ron.craig@comcast.net>
2018-09-13 02:11:17 -0700

    add player 1-2 interface and result in the terminal

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,8 +1,16 @@
+#!/usr/bin/env ruby
+require('pry')
+
+puts 'Player 1, pick your item: rock, paper or scissors'
+@p1 = gets.chomp
+
+# items = [rock, paper, scissors]
+# random_item = items.sample
+puts 'Player 2, pick your item: rock, paper or scissors'
+@p2 = gets.chomp
+
 class RockPaperScissors
   def initialize(p1, p2)
-    #!/usr/bin/env ruby
-    # puts 'Player 1, pick your item: rock, paper or scissors'
-    # game = gets.chomp
     @p1 = p1
     @p2 = p2
   end
@@ -10,14 +18,16 @@ class RockPaperScissors
   def wins?(player1, player2)
     player1 = @p1
     player2 = @p2
-    # major_objects = ['rock', 'paper', 'scissors']
-    # player2 = major_objects.sample
     if (player1 == 'rock') & (player2 == 'scissors') | (player1 == 'paper') & (player2 == 'rock') | (player1 == 'scissors') & (player2 == 'paper')
-      true
+      'Player 1 wins'
     elsif (player1 == 'scissors') & (player2 == 'rock') | (player1 == 'rock') & (player2 == 'paper') | (player1 == 'paper') & (player2 == 'scissors')
-      false
+      'Player 2 wins'
     else
-      'tie'
+      'This is a tie'
     end
   end
 end
+
+game = RockPaperScissors.new(@p1, @p2)
+the_end = game.wins?(@p1, @p2)
+puts "#{the_end}!"
