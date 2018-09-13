be676bff3c30befc4031c04092d051bd0c887db9
Ron Craig <ron.craig@comcast.net>
2018-09-12 23:41:10 -0700

    add two arguments in the initialize method, change if branches to true, false, 'tie'

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,24 +1,23 @@
-class RPS
-
-  def initialize(game)
+class RockPaperScissors
+  def initialize(p1, p2)
     #!/usr/bin/env ruby
-    puts "Player 1, pick your item: rock, paper or scissors"
-    game = gets.chomp
-    @game = game
+    # puts 'Player 1, pick your item: rock, paper or scissors'
+    # game = gets.chomp
+    @p1 = p1
+    @p2 = p2
   end
 
-  def wins(player1, player2)
-    player1 = @game
-    if ((player1 == "rock") & (player2 == "scissors"))
-      "player 1 wins"
-    elsif ((player1 == "paper") & (player2 == "rock"))
-      "player 1 wins"
-    elsif ((player1 == "scissors") & (player2 == "paper"))
-      "player 1 wins"
-    elsif (player1 == player2)
-      "it's a tie"
+  def wins?(player1, player2)
+    player1 = @p1
+    player2 = @p2
+    # major_objects = ['rock', 'paper', 'scissors']
+    # player2 = major_objects.sample
+    if (player1 == 'rock') & (player2 == 'scissors') | (player1 == 'paper') & (player2 == 'rock') | (player1 == 'scissors') & (player2 == 'paper')
+      true
+    elsif (player1 == 'scissors') & (player2 == 'rock') | (player1 == 'rock') & (player2 == 'paper') | (player1 == 'paper') & (player2 == 'scissors')
+      false
     else
-      "player 2 wins"
+      'tie'
     end
   end
 end
