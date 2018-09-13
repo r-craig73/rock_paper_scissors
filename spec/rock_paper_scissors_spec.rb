b2f8ccf648669fd66debd73e7b5790def1de6bd9
Ron Craig <ron.craig@comcast.net>
2018-09-13 02:11:43 -0700

    add new text to announce a win or tie

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -2,41 +2,41 @@ require('rspec')
 require('pry')
 require('rock_paper_scissors')
 
-describe('#rock_paper_scissors') do
-  it('returns true if rock is the object and scissors is the argument') do
+describe('rock_paper_scissors') do
+  it('selects player 1 if rock is the object and scissors is the argument') do
     game = RockPaperScissors.new('rock', 'scissors')
-    expect(game.wins?('rock', 'scissors')).to(eq(true))
+    expect(game.wins?('rock', 'scissors')).to(eq('Player 1 wins'))
   end
-  it('returns true if paper is the object and rock is the argument') do
+  it('selects player 1 if paper is the object and rock is the argument') do
     game = RockPaperScissors.new('paper', 'rock')
-    expect(game.wins?('paper', 'rock')).to(eq(true))
+    expect(game.wins?('paper', 'rock')).to(eq('Player 1 wins'))
   end
-  it('returns true if scissors is the object and paper is the argument') do
+  it('selects player 1 if scissors is the object and paper is the argument') do
     game = RockPaperScissors.new('scissors', 'paper')
-    expect(game.wins?('scissors', 'paper')).to(eq(true))
+    expect(game.wins?('scissors', 'paper')).to(eq('Player 1 wins'))
   end
   it('returns a tie if player 1 scissors matches the object and argument') do
     game = RockPaperScissors.new('scissors', 'scissors')
-    expect(game.wins?('scissors', 'scissors')).to(eq('tie'))
+    expect(game.wins?('scissors', 'scissors')).to(eq('This is a tie'))
   end
   it('returns a tie if player 1 paper matches the object and argument') do
     game = RockPaperScissors.new('paper', 'paper')
-    expect(game.wins?('paper', 'paper')).to(eq('tie'))
+    expect(game.wins?('paper', 'paper')).to(eq('This is a tie'))
   end
   it('returns a tie if player 1 rock matches the object and argument') do
     game = RockPaperScissors.new('rock', 'rock')
-    expect(game.wins?('rock', 'rock')).to(eq('tie'))
+    expect(game.wins?('rock', 'rock')).to(eq('This is a tie'))
   end
-  it('returns false if rock is the object and paper is the argument') do
+  it('selects player 2 if rock is the object and paper is the argument') do
     game = RockPaperScissors.new('rock', 'paper')
-    expect(game.wins?('rock', 'paper')).to(eq(false))
+    expect(game.wins?('rock', 'paper')).to(eq('Player 2 wins'))
   end
-  it('returns false if paper is the object and scissors is the argument') do
+  it('selects player 2 if paper is the object and scissors is the argument') do
     game = RockPaperScissors.new('paper', 'scissors')
-    expect(game.wins?('paper', 'scissors')).to(eq(false))
+    expect(game.wins?('paper', 'scissors')).to(eq('Player 2 wins'))
   end
-  it('returns false if scissors is the object and rock is the argument') do
+  it('selects player 2 if scissors is the object and rock is the argument') do
     game = RockPaperScissors.new('scissors', 'rock')
-    expect(game.wins?('scissors', 'rock')).to(eq(false))
+    expect(game.wins?('scissors', 'rock')).to(eq('Player 2 wins'))
   end
 end
