1c0aaa322176aae7affcdac06dbf6ab8b937d6ae
Ron Craig <ron.craig@comcast.net>
2019-03-06 22:38:13 -0800

    edit shebang line and change Tie output phrase

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -15,18 +15,20 @@ class RockPaperScissors
     elsif (player1 == 'scissors') & (player2 == 'rock') | (player1 == 'rock') & (player2 == 'paper') | (player1 == 'paper') & (player2 == 'scissors')
       'Computer wins'
     elsif player1 == player2
-      'This is a tie'
+      "How about that? It's a tie!"
     else
       'Oh boy! Did someone dropped their item or misspelled an item?'
     end
   end
 end
 
+puts 'It is time to play RPS! Rock, paper or scissors'
 puts 'Player 1, pick your item: rock, paper or scissors'
 @p1 = gets.chomp
 
 items = %w[rock paper scissors] # similar to ['rock', 'paper', 'scissors']
 random_item = items.sample
+sleep 2.0
 puts "Computer selects #{random_item}"
 @p2 = random_item
 
